9d4e550b9ec1fed6bcfed24fb1a505023284b64d
Thomas Batteux <tbatteux@c1r7p10.42lehavre.fr>
2024-04-09 16:22:30 +0200

    submodule

--- options.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-void    help_options()
-{
-    printf("Usage: ./ft_traceroute [OPTION...] HOST\n");
-    printf("Print the route packets trace to network host.\n\n");
-    printf("   -?, --help\t\tgive this help list\n\n");
-}
--- utils.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-tr  init_struct_tr()
-{
-    tr  new;
-    new.help = false;
-    return new;
-}
